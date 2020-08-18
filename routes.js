203e1a60a3989f9968bbc5a9014998bf49c98c40
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-18 11:24:13 -0600

    Se agrega funcionalidad para el boton de elegir respuesta correcta, y se protegen las unas rutas para los usuarios logueados

--- routes.js
+++ routes.js
@@ -62,7 +62,11 @@ module.exports =
     },    
     handler : question.answerQuestion,
 },
-
+{
+    method: 'GET',
+    path : '/answer/{questionId}/{answerId}',
+    handler : question.setAnswerRight
+},
 {
     method: 'POST',
     path : '/create-question',
